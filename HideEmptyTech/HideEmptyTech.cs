ac049e21dce1e31921e8a574223d8ea35ce6ae6c
nanathan <rosenprog@yahoo.com>
2015-06-26 12:18:57 -0400

    Initial implementation to hide all tech nodes that have no parts associated

---
+++ HideEmptyTech/HideEmptyTech.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+
+namespace HideEmptyTech
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class HideEmptyTechAddon : MonoBehaviour
+    {
+        public void Awake()
+        {
+            Debug.Log("Awake");
+            RDTechTree.OnTechTreeSpawn.Add(new EventData<RDTechTree>.OnEvent(onTechTreeSpawn));
+        }
+
+        public void Start()
+        {
+            Debug.Log("Start()");
+            DontDestroyOnLoad(this);
+        }
+
+        public void onTechTreeSpawn(RDTechTree techTree)
+        {
+            if(techTree != null)
+            {
+                hideNodes(techTree);
+            }
+        }
+
+        public void hideNodes(RDTechTree techTree)
+        {
+            foreach(RDNode node in techTree.controller.nodes)
+            {
+                Debug.Log("Node " + node.name + " " + node.PartsInTotal());
+                node.gameObject.SetActive(node.PartsInTotal() > 0);
+            }
+        }
+    }
+}
